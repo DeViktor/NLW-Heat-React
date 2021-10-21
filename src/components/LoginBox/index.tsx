041d42b0f5d5b67599b4ab8c104de5abd4b22e4c
DeViktor <contato.4years@gmail.com>
2021-10-20 20:44:35 -0400

    card with sign in button added

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,8 +1,14 @@
 import styles from './styles.module.scss'
+import { FaGithub } from 'react-icons/fa';
+
 export function LoginBox() {
     return (
         <div className={styles.loginBoxWrapper}>
-            <strong></strong>
+            <strong>Enter and Share</strong>
+            <a href="" className={styles.signIn}>
+                <FaGithub size="22" />
+                Sign In With GitHub
+            </a>
         </div>
     )
 }
