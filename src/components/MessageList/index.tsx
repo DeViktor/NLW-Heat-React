d8616fb2d6cb40d069b0bbcd4a9442e6cde4a429
DeViktor <contato.4years@gmail.com>
2021-10-20 22:04:19 -0400

    Base Front as Been Finished

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,40 @@
+import styles from './style.module.scss'
+
+import img from '../../assets/logo.svg'
+
 export function MessageList() {
     return (
-        <h1>Message</h1>
+        <div className={styles.messageWrapper}>
+            <img src={img} alt="DoWhile 2021" />
+            <ul className={styles.messageList}>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}> minha incrivel jiromba</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/DeViktor.png" alt="Farinhas Farinhoso" />
+                        </div>
+                        <span>Farinhas Farinhoso</span>
+                    </div>
+                </li>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}> minha incrivel jiromba</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/DeViktor.png" alt="Farinhas Farinhoso" />
+                        </div>
+                        <span>Farinhas Farinhoso</span>
+                    </div>
+                </li>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}> minha incrivel jiromba</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/DeViktor.png" alt="Farinhas Farinhoso" />
+                        </div>
+                        <span>Farinhas Farinhoso</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
     )
 }
